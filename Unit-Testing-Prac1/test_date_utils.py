aa183f98704c9aee4329ad67f9782ae932556e96
l00188531 <l00188531@atu.ie>
2024-10-16 04:15:30 +0100

    feat (dropdown-filter) more until tests added in test_date_utils.py   Related to #2

--- Unit-Testing-Prac1/test_date_utils.py
+++ Unit-Testing-Prac1/test_date_utils.py
@@ -14,6 +14,10 @@ class TestDateUtils(unittest.TestCase):
     def test_invalid_date_format(self):
         # Invalid date format
         self.assertFalse(is_valid_date("15-05-2022"))
+    
+    def test_edge_case_start_date(self):
+        # Valid date exactly on the start date
+        self.assertTrue(is_valid_date("2020-01-01"))
 
 
 if __name__ == '__main__':
