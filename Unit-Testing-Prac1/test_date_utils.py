4c9c7999bb3ca83fcd3e7a53c62810bb8d68e236
l00188531 <l00188531@atu.ie>
2024-10-16 04:12:41 +0100

    feat (dropdown-filter) test_date_utils.py added (Unit Test for is_valid_date)  Related to #2

---
+++ Unit-Testing-Prac1/test_date_utils.py
@@ -0,0 +1,20 @@
+import unittest
+from date_utils import is_valid_date
+
+class TestDateUtils(unittest.TestCase):
+
+    def test_valid_date_in_range(self):
+        # Valid date within range
+        self.assertTrue(is_valid_date("2022-05-15"))
+
+    def test_valid_date_out_of_range(self):
+        # Valid date but out of range
+        self.assertFalse(is_valid_date("2024-01-01"))
+
+    def test_invalid_date_format(self):
+        # Invalid date format
+        self.assertFalse(is_valid_date("15-05-2022"))
+
+
+if __name__ == '__main__':
+    unittest.main()
