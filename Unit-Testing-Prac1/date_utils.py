e2e921b9b957412741949484b3b92706677f9acc
l00188531 <l00188531@atu.ie>
2024-10-16 04:02:16 +0100

    feat (dropdown-filter) python function updated for date validation  Related to #2

--- Unit-Testing-Prac1/date_utils.py
+++ Unit-Testing-Prac1/date_utils.py
@@ -6,8 +6,9 @@ def is_valid_date(date_str):
     :return: True if the date is valid and within the range, False otherwise
     """
     try:
-        #code 
-
-        return 
+        date = datetime.strptime(date_str, "%Y-%m-%d")
+        start_date = datetime(2020, 1, 1)
+        end_date = datetime(2023, 12, 31)
+        return start_date <= date <= end_date
     except ValueError:
         return False
